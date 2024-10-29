879addc37c288537cc23156a023b76ab02019e0c
Maxim Fadeev <maksimfadeev4673@gmail.com>
2024-10-29 23:48:01 +0300

    It's a first commit: add basic configuration and dependencies

---
+++ src/main/java/ru/max/springcourse/HelloController.java
@@ -0,0 +1,16 @@
+package ru.max.springcourse;
+
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
