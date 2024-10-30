b676d727d5440336b87f8c790ac359a826e2041a
Maxim Fadeev <maksimfadeev4673@gmail.com>
2024-10-30 23:58:02 +0300

    add tag in hello.html, set method helloPage() in FirstController

--- src/main/java/ru/max/springcourse/controllers/FirstController.java
+++ src/main/java/ru/max/springcourse/controllers/FirstController.java
@@ -1,7 +1,7 @@
 package ru.max.springcourse.controllers;
 
 
-import org.springframework.lang.Nullable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
